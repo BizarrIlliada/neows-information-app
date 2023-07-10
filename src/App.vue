4e979a572d9efc7060604494d5843db2a2d07d55
BizarrIlliada <kazkoviy@icloud.com>
2023-07-10 21:24:59 +0300

    element-plus UI-library was added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ElButton>Start</ElButton>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ElButton } from 'element-plus'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: { ElButton }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {
+  ElButton,
+} from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/styles/main.scss'
+
+const app = createApp(App)
+
+const elementsPlus = [
+  ElButton,
+]
+
+elementsPlus.forEach(el => {
+  app.component(el.name, el)
+})
+
+app.mount('#app')
